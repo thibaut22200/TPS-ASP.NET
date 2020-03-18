9baa7dba82fa57627c977f63e5b8f0d91d2cf4ae
thibaut22200 <thouement.t@outlook.fr>
2020-03-18 12:25:06 +0100

    Add TP01Module05

---
+++ TP01Module05/Controllers/ChatController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TP01Module05.Models;
+
+namespace TP01Module05.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<Chat> chats = Chat.GetMeuteDeChats();
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View(chats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            Chat chat = getChatById(id);
+            if(chat == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(chat);
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Chat chat = getChatById(id);
+            if (chat != null)
+            {
+                return View(chat);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                Chat chat = getChatById(id);
+                if (chat != null)
+                {
+                    chats.Remove(chat);
+                }
+            }
+            catch
+            {
+                return View();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        // METHODES PRIVEES
+        private Chat getChatById(int id)
+        {
+            var chat = chats.FirstOrDefault(c => c.Id == id);
+            return chat;
+        }
+    }
+}
---
+++ TP01Module05/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TP01Module05.MvcApplication" Language="C#" %>
